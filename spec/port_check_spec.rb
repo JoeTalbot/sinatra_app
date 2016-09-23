679515f731640abf42a4c1f658f6dc0ecbf52bcc
Joe Talbot <joe.talbot@target.com>
2016-09-23 14:50:31 -0500

    changed to use port checking instead

--- spec/port_check_spec.rb
+++ spec/port_check_spec.rb
@@ -1,17 +1,25 @@
-require_relative '../reverse.rb'
+require_relative '../port_check.rb'
 require 'rack/test'
 require 'simplecov'
+require 'capybara/rspec'
+require 'rspec-mocks'
 
 SimpleCov.start
 set :environment, :test
+require ::File.expand_path('../../port_check', __FILE__)
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+end
 
 def app
     Sinatra::Application
 end
 
 describe 'check port' do
-    include Rack::Test::Methods
+  is_port_open? = double("is_port_open?")
     it "should report if the localhost port is open" do
+
         get '/check_ports/ip/localhost/port/4567'
         last_response.body.should == 'The port is open'
     end
